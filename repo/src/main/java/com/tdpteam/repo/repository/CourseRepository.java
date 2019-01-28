49ec4426d6d925c01221e93ad43827848344bdc7
Duy Nguyen <milonguyen95@outlook.com>
2019-01-28 15:54:59 +0700

    Create Course repository

---
+++ repo/src/main/java/com/tdpteam/repo/repository/CourseRepository.java
@@ -0,0 +1,9 @@
+package com.tdpteam.repo.repository;
+
+import com.tdpteam.repo.entity.Course;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CourseRepository extends JpaRepository<Course, Long> {
+}
