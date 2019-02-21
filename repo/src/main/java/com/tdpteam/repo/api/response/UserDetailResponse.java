3bbabf69c3a0da2328e3fff5536ef9af2b2e06ea
Duy Nguyen <milonguyen95@outlook.com>
2019-02-21 12:09:47 +0700

    update api to use first name and last name

--- repo/src/main/java/com/tdpteam/repo/api/response/UserDetailResponse.java
+++ repo/src/main/java/com/tdpteam/repo/api/response/UserDetailResponse.java
@@ -10,5 +10,6 @@ import lombok.*;
 public class UserDetailResponse {
     private String email;
     private String phone;
-    private String fullName;
+    private String firstName;
+    private String lastName;
 }
