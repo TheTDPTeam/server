23cebe3ed3fd39fd9245119f51485826534d77c1
Duy Nguyen <milonguyen95@outlook.com>
2019-02-21 12:33:51 +0700

    Update user details

--- service/src/main/java/com/tdpteam/service/impl/UserServiceImpl.java
+++ service/src/main/java/com/tdpteam/service/impl/UserServiceImpl.java
@@ -1,24 +1,37 @@
 package com.tdpteam.service.impl;
 
+import com.tdpteam.repo.api.request.UpdateDetailRequest;
 import com.tdpteam.repo.api.response.UserDetailResponse;
 import com.tdpteam.repo.entity.user.Account;
+import com.tdpteam.repo.entity.user.UserDetail;
+import com.tdpteam.repo.repository.AccountRepository;
 import com.tdpteam.service.interf.AccountService;
 import com.tdpteam.service.interf.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class UserServiceImpl implements UserService {
-    private AccountService accountService;
+    private AccountRepository accountRepository;
+    private BCryptPasswordEncoder encoder;
 
     @Autowired
-    public UserServiceImpl(AccountService accountService) {
-        this.accountService = accountService;
+    public UserServiceImpl(AccountRepository accountRepository,
+                           BCryptPasswordEncoder encoder) {
+        this.accountRepository = accountRepository;
+        this.encoder = encoder;
     }
 
     @Override
     public UserDetailResponse getUserDetailById(Long id) {
-        Account account = accountService.findAccountById(id);
+        Optional<Account> optionalAccount = accountRepository.findById(id);
+        if(!optionalAccount.isPresent()) return null;
+        Account account = optionalAccount.get();
         return UserDetailResponse
                 .builder()
                 .email(account.getEmail())
@@ -26,5 +39,28 @@ public class UserServiceImpl implements UserService {
                 .firstName(account.getUserDetail().getFirstName())
                 .lastName(account.getUserDetail().getLastName())
                 .build();
+    }
+
+    @Override
+    public void changePassword(Account account, String password) {
+        account.setPassword(encoder.encode(password));
+        accountRepository.save(account);
+    }
+
+    @Override
+    public ResponseEntity<UserDetailResponse> updateUserDetail(Account account, UpdateDetailRequest data) {
+        UserDetail userDetail = account.getUserDetail();
+        userDetail.setLastName(data.getLastName());
+        userDetail.setFirstName(data.getFirstName());
+        userDetail.setPhone(data.getPhone());
+        account.setUserDetail(userDetail);
+        Account updatedAccount = accountRepository.save(account);
+        return new ResponseEntity<>(UserDetailResponse
+                .builder()
+                .email(updatedAccount.getEmail())
+                .phone(updatedAccount.getUserDetail().getPhone())
+                .firstName(updatedAccount.getUserDetail().getFirstName())
+                .lastName(updatedAccount.getUserDetail().getLastName())
+                .build(), HttpStatus.OK);
     }
 }
